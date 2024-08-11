c5564e2b2932f7b44fed66ff197760619538e5d0
Sebastian Melo Ruiz <cursossebas2028@gmail.com>
2024-08-11 14:11:08 -0500

    Drawers interactables

--- Assets/Scripts/TableSubject.cs
+++ Assets/Scripts/TableSubject.cs
@@ -3,16 +3,31 @@ using UnityEngine;
 
 public class TableSubject : MonoBehaviour
 {
+    public enum ObjectType { Drawer, Door }
+    public ObjectType objectType;
     public bool isOpen;
     [SerializeField]Transform drawer;
     public enum Orientation {x, y, z};
     public Orientation orientation;
     public float originValue, destinationValue;
     
-
     public void OpenClose()
     {
         isOpen = !isOpen;
+
+        switch (objectType)
+        {
+            case ObjectType.Drawer:
+                HandleDrawer();
+                break;
+            case ObjectType.Door:
+                HandleDoor();
+                break;
+        }
+    }
+
+    public void HandleDrawer()
+    {
         if(isOpen)
         {
             switch (orientation)
@@ -51,6 +66,52 @@ public class TableSubject : MonoBehaviour
                 case Orientation.z:
                     
                     drawer.localPosition = new Vector3(drawer.localPosition.x,drawer.localPosition.y, originValue);
+
+                break;
+            }
+        }
+    }
+
+    public void HandleDoor()
+    {
+        if(isOpen)
+        {
+            switch (orientation)
+            {
+                case Orientation.x:
+                    
+                    drawer.localRotation = Quaternion.Euler(destinationValue, drawer.localRotation.eulerAngles.y, drawer.localRotation.eulerAngles.z);
+
+                break;
+                case Orientation.y:
+                    
+                    drawer.localRotation = Quaternion.Euler(drawer.localRotation.eulerAngles.x, destinationValue, drawer.localRotation.eulerAngles.z);
+
+                break;
+                case Orientation.z:
+                    
+                    drawer.localRotation = Quaternion.Euler(drawer.localRotation.eulerAngles.x, drawer.localRotation.eulerAngles.y, destinationValue);
+
+                break;
+            }
+        }
+        else{
+            
+            switch (orientation)
+            {
+                case Orientation.x:
+                    
+                    drawer.localRotation = Quaternion.Euler(drawer.localRotation.eulerAngles.x, drawer.localRotation.eulerAngles.y, destinationValue);
+
+                break;
+                case Orientation.y:
+                    
+                    drawer.localRotation = Quaternion.Euler(drawer.localRotation.eulerAngles.x, originValue, drawer.localRotation.eulerAngles.z);
+
+                break;
+                case Orientation.z:
+                    
+                    drawer.localRotation = Quaternion.Euler(drawer.localRotation.eulerAngles.x, drawer.localRotation.eulerAngles.y, originValue);
 
                 break;
             }
